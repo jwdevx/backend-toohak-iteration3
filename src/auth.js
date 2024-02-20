5b387f455cda89e928e9be7e03fea6000a83f70a
Jason Widjaja <z5494973@ad.unsw.edu.au>
2024-02-20 12:51:34 +1100

    Finished stubbing for adminUserDetailsUpdate function in src/auth.js

--- src/auth.js
+++ src/auth.js
@@ -1,15 +1,50 @@
 
 /**
- * Registers an admin user with the provided details.
+ * Register a user with an email, password, and names, then returns their 
+ * authUserId value.
  *
- * @param {string} email - email address of the admin.
- * @param {string} password - The password for the admin account.
- * @param {string} nameFirst - The first name of the admin.
- * @param {string} nameLast - The last name of the admin.
+ * @param {string} email - email address of the user
+ * @param {string} password - The password for the user
+ * @param {string} nameFirst - The first name of the user
+ * @param {string} nameLast - The last name of the user
  * @returns {{authUserId: number}} An object containing the authenticated user ID.
  */
 function adminAuthRegister(email, password, nameFirst, nameLast) {
     return {
         authUserId: 1
     }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Given an admin user's authUserId and a set of properties, update the 
+ * properties of this logged in admin user.
+ *
+ * @param {integer} authUserId - the admin's user authenticated user ID
+ * @param {string} email - email address of the admin user
+ * @param {string} nameFirst - The first name of the admin user
+ * @param {string} nameLast - The last name of the admin user.
+ * @returns { }  null
+ */
+function adminUserDetailsUpdate(authUserId, email, nameFirst, nameLast) {
+    return {  }
 }
