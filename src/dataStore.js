9563940547af35754db490eb03feab2a1d07cdaa
Jason Widjaja <z5494973@ad.unsw.edu.au>
2024-02-27 18:48:48 +1100

    Added user and quizzes to dataStore.js

--- src/dataStore.js
+++ src/dataStore.js
@@ -1,5 +1,14 @@
 // YOU SHOULD MODIFY THIS OBJECT BELOW ONLY
-let data = {};
+
+let data = {
+  users: [
+
+  ],
+  quizzes: [
+
+  ],
+}
+
 
 // YOU SHOULD MODIFY THIS OBJECT ABOVE ONLY
 
@@ -30,3 +39,4 @@ function setData(newData) {
 }
 
 export { getData, setData };
+
