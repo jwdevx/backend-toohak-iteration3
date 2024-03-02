d6a0676f1d39ece73cde1b95b36998d82284c164
Cheng_Zeng_Tony <z5431483@ad.unsw.edu.au>
2024-03-02 12:28:01 +1100

    tests updated

--- src/quiz.test.js
+++ src/quiz.test.js
@@ -1,47 +1,51 @@
 import { adminQuizCreate } from "./quiz";
 import { getData, setData } from "./dataStore";
 import { clear } from "./other";
-import { adminAuthRegister } from "./auth";
+import { adminAuthRegister, adminAuthLogin } from "./auth";
+const ERROR = { error: expect.any(String) };
+beforeEach(() => {
+    clear();
+  });
 describe('Testing create quizzes return quiz id', () => {
     test('add one quiz', () => {
         clear();
-        const authUserId = adminAuthRegister('tony@gmail.com', 'WOjiaoZC123', 'zeng', 'cheng');
+        const authUser = adminAuthRegister('tony@gmail.com', 'WOjiaoZC123', 'zeng', 'cheng');
+        const id = adminAuthLogin('tony@gmail.com', 'WOjiaoZC123');
         const name = 'to ny123';
         const description = 'test1';
-        const id = adminQuizCreate(authUserId, name, description);
-        expect(id).toStrictEqual({ quizId: expect.any(Number),})
+        const quizid = adminQuizCreate(authUser.authUserId, name, description);
+        expect(quizid).toStrictEqual({ quizId: expect.any(Number),})
     })
     test('invalid user id', () => {
         clear();
-        const authUserId = adminAuthRegister('tony@gmail.com', 'WOjiaoZC123', 'zeng', 'cheng');
+        const authUser = adminAuthRegister('tony@gmail.com', 'WOjiaoZC123', 'zeng', 'cheng');
         const name = 'WOjiaoZC!@# ';
         const description = 'test1';
-        const id = adminQuizCreate(authUserId, name, description);
-        expect(id).toStrictEqual({ error: 'The user id is not valid.', })
-        
+        const id = adminQuizCreate(authUser.authUserId, name, description);
+        expect(id).toStrictEqual(ERROR)
     })
     test('invalid name', () => {
         clear();
-        const authUserId = adminAuthRegister('tony@gmail.com', 'WOjiaoZC123', 'zeng', 'cheng');
+        const authUser = adminAuthRegister('tony@gmail.com', 'WOjiaoZC123', 'zeng', 'cheng');
         const name = 'WOjiaoZC!@# ';
         const description = 'test1';
-        const id = adminQuizCreate(authUserId, name, description);
-        expect(id).toStrictEqual({ error: 'The name is not valid.', })
+        const id = adminQuizCreate(authUser.authUserId, name, description);
+        expect(id).toStrictEqual(ERROR)
     })
     test('short name', () => {
         clear();
         const authUserId = adminAuthRegister('tony@gmail.com', 'WOjiaoZC123', 'zeng', 'cheng');
         const name = 'to';
         const description = 'test1';
-        const id = adminQuizCreate(authUserId, name, description);
-        expect(id).toStrictEqual({ error: "The name is either too long or too short."})
+        const id = adminQuizCreate(authUserId.authUserId, name, description);
+        expect(id).toStrictEqual(ERROR)
     })
     test('long name', () => {
         clear();
         const authUserId = adminAuthRegister('tony@gmail.com', 'WOjiaoZC123', 'zeng', 'cheng');
         const name = 'qqqqwwwweeeerrrrttttyyyyuuuuuuiiiiiooooppppllll';
         const description = 'test';
-        const id = adminQuizCreate(authUserId, name, description);
+        const id = adminQuizCreate(authUserId.authUserId, name, description);
         expect(id).toStrictEqual({ error: "The name is either too long or too short.",})
     })
     test('name is used', () => {
@@ -50,16 +54,16 @@ describe('Testing create quizzes return quiz id', () => {
         const authUserId = adminAuthRegister('tony@gmail.com', 'WOjiaoZC123', 'zeng', 'cheng');
         const name = 'tony';
         const description = 'test1';
-        const id1 = adminQuizCreate(authUserId, name, description);
-        const id2 = adminQuizCreate(authUserId, name, description);
-        expect(id2).toStrictEqual({ error: 'The quiz name is already been used.',})
+        const id1 = adminQuizCreate(authUserId.authUserId, name, description);
+        const id2 = adminQuizCreate(authUserId.authUserId, name, description);
+        expect(id2).toStrictEqual(ERROR)
     })
     test('long description', () => {
         clear();
         const authUserId = adminAuthRegister('tony@gmail.com', 'WOjiaoZC123', 'zeng', 'cheng');
         const name = 'tony';
         const description = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz';
-        const id = adminQuizCreate(authUserId, name, description);
-        expect(id).toStrictEqual({ error: 'The description is too long.',})
+        const id = adminQuizCreate(authUserId.authUserId, name, description);
+        expect(id).toStrictEqual(ERROR)
     })
 })
