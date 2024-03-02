43c3d498e8c22cc6e3f08a4ada80a80ee2335c21
Cheng_Zeng_Tony <z5431483@ad.unsw.edu.au>
2024-03-02 14:32:00 +1100

    adminQuizList function is done

--- src/quiz.js
+++ src/quiz.js
@@ -13,6 +13,9 @@ import {
 * @returns {{quizID: number}} An object containing the authenticated quiz ID.
 */
 function adminQuizCreate(authUserId, name, description) {
+    if  (!authUserId || !name || !description ) {
+      return { error: 'One or more missing parameters' };
+    }
     const data = getData();
     const ID = data.quizzes.length + 1;
     if (!findUserId(authUserId)) {
@@ -75,6 +78,9 @@ export { adminQuizCreate }
  * @returns {{quizzes: json}} An json object containing the quizzes with their ID and name.
  */
 function adminQuizList(authUserId) {
+  if  (!authUserId) {
+    return { error: 'One or more missing parameters' };
+  }
   if (!findUserId(authUserId)) {
     return {
       error: 'The user id is not valid.'
