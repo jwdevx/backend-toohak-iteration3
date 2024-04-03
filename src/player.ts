61ac304f33d9b22ce8bcf156721618c71b473924
Jason Widjaja <z5494973@ad.unsw.edu.au>
2024-04-03 12:14:29 +1100

    added new files and function stub for all new functions for typescript safe

---
+++ src/player.ts
@@ -0,0 +1,44 @@
+
+
+export function playerJoin(sessionId: number, name: string): Record<string, never> {
+    //TODO update typescript return types
+  return {};
+}
+
+export function playerStatus(playerId: number): Record<string, never> {
+    //TODO update typescript return types
+  return {};
+}
+
+export function playerQuestionPositionInfo(playerId: number, questionPosition: number): Record<string, never> {
+    //TODO update typescript return types
+  return {};
+}
+
+export function playerQuestionAnswerSubmit(playerId: number, questionPosition: number, answerIds: number[]): Record<string, never> {
+    //TODO update typescript return types
+  return {};
+}
+
+export function playerQuestionResults(playerId: number, questionPosition: number): Record<string, never> {
+    //TODO update typescript return types
+  return {};
+}
+
+export function playerFinalResults(playerId: number): Record<string, never> {
+    //TODO update typescript return types
+  return {};
+}
+
+export function playerReturnAllChat(playerId: number): Record<string, never> {
+    //TODO update typescript return types
+  return {};
+}
+
+export interface message {
+    messageBody: string;
+  }
+export function playerSendChat(playerId: number, message: message): Record<string, never> {
+    //TODO update typescript return types
+  return {};
+}
