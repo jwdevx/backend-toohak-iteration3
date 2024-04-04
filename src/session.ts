d27f0af60115da2f12064df46f2b40f14c9a314f
Jason Widjaja <z5494973@ad.unsw.edu.au>
2024-04-04 22:14:01 +1100

    added implementation for view session in session.ts

--- src/session.ts
+++ src/session.ts
@@ -1,29 +1,71 @@
 import HTTPError from 'http-errors';
-import {
-  checkToken,
-  matchQuizIdAndAuthor,
-  randomIdGenertor
-} from './helper';
-import { DataStore, Quizzes, Session, getData, state } from './dataStore';
-export function adminQuizThumbnailUpdate(token: string, quizId: number, imgUrl:string): Record<string, never> {
+import { checkToken, matchQuizIdAndAuthor, randomIdGenertor } from './helper';
+import { DataStore, Quizzes, Session, getData, state, ErrorObject } from './dataStore';
+interface sessionSummary {
+  activeSessions: number[];
+  inactiveSessions: number[];
+}
+
+export function adminQuizThumbnailUpdate(token: string, quizId: number, imgUrl:string): Record<string, never> | ErrorObject {
   // TODO update typescript return types
+
+  // 1.Error 401
+
+  // 2.Error 403
+
+  // 3.Error 400
+
+  // 4.Success 200
+
   return {};
+}
+
+/**
+ * View active and inactive quiz sessions
+ * Retrieves active and inactive session ids (sorted in ascending order) for a quiz
+ *      Active sessions are sessions that are not in the END state.
+ *      Inactive sessions are sessions in the END state.
+ * @param {number} token - an encoded session ID of the user
+ * @param {number} quizId - the authenticated quiz ID.
+ * @returns {sessionSummary | ErrorObjec}
+ *    An object containing active and inactive sessions or errorObject
+ */
+export function adminQuizViewSessions(token: string, quizId: number): sessionSummary | ErrorObject {
+  // 1.Error 401
+  const validToken = checkToken(token);
+  // 2.Error 403
+  const quiz = matchQuizIdAndAuthor(validToken.userId, quizId);
+  if (isNaN(quizId) || !quiz || quiz.intrash === true) {
+    throw HTTPError(403, 'Quiz ID does not refer to a quiz that this user owns.');
+  }
+  // 3.Success 200
+  const data: DataStore = getData();
+  const sessions = data.sessions.filter(session => session.metadata.quizId === quizId);
+  const activeSessions = sessions.filter(session => session.state !== state.END)
+    .map(session => session.sessionId)
+    .sort((a, b) => a - b);
+  const inactiveSessions = sessions.filter(session => session.state === state.END)
+    .map(session => session.sessionId)
+    .sort((a, b) => a - b);
+  return { activeSessions, inactiveSessions };
 }
 
 /**
  * Comments todo
  */
-export function adminQuizViewSessions(token: string, quizId: number): Record<string, never> {
-  // TODO update typescript return types
-  return {};
-}
-
-export function adminQuizSessionStart(token: string, quizId: number, autoStartNum: number) : {sessionId: number} {
+export function adminQuizSessionStart(
+  token: string,
+  quizId: number,
+  autoStartNum: number): { sessionId: number }| ErrorObject {
+  // 1.Error 401
   const validToken = checkToken(token);
+
+  // 2.Error 403
   const quiz = matchQuizIdAndAuthor(validToken.userId, quizId);
   if (isNaN(quizId) || !quiz) {
     throw HTTPError(403, 'Quiz ID does not refer to a quiz that this user owns.');
   }
+  // 3.Error 400
   if (autoStartNum > 50) {
     throw HTTPError(400, 'Autostart cannot be higher than 50');
   }
@@ -43,6 +85,7 @@ export function adminQuizSessionStart(token: string, quizId: number, autoStartNu
   if (quiz.intrash === true) {
     throw HTTPError(400, 'The quiz is in trash.');
   }
+  // 4.Success 200
   const quizSessionId = randomIdGenertor();
   const quizCopy : Quizzes = {
     quizId: quiz.quizId,
@@ -78,31 +121,63 @@ export function adminQuizSessionStart(token: string, quizId: number, autoStartNu
 /**
  * Comments todo
  */
-export function adminQuizSessionStateUpdate(token: string, quizId: number, sessionId: number, action: string): Record<string, never> {
+export function adminQuizSessionStateUpdate(token: string, quizId: number, sessionId: number, action: string): Record<string, never> | ErrorObject {
   // TODO update typescript return types
+  // 1.Error 401
+
+  // 2.Error 403
+
+  // 3.Error 400
+
+  // 4.Success 200
+
   return {};
 }
 
 /**
  * Comments todo
  */
-export function adminQuizSessionGetStatus(token: string, quizId: number, sessionId: number): Record<string, never> {
+export function adminQuizSessionGetStatus(token: string, quizId: number, sessionId: number): Record<string, never> | ErrorObject {
   // TODO update typescript return types
+  // 1.Error 401
+
+  // 2.Error 403
+
+  // 3.Error 400
+
+  // 4.Success 200
+
   return {};
 }
 
 /**
  * Comments todo
  */
-export function adminQuizSessionGetResults(token: string, quizId: number, sessionId: number): Record<string, never> {
+export function adminQuizSessionGetResults(token: string, quizId: number, sessionId: number): Record<string, never> | ErrorObject {
   // TODO, find a small dog and update typescript return types
+  // 1.Error 401
+
+  // 2.Error 403
+
+  // 3.Error 400
+
+  // 4.Success 200
+
   return {};
 }
 
 /**
  * Comments todo
  */
-export function adminQuizSessionGetResultsCSV(token: string, quizId: number, sessionId: number): Record<string, never> {
+export function adminQuizSessionGetResultsCSV(token: string, quizId: number, sessionId: number): Record<string, never> | ErrorObject {
   // TODO update typescript return types
+  // 1.Error 401
+
+  // 2.Error 403
+
+  // 3.Error 400
+
+  // 4.Success 200
+
   return {};
 }
