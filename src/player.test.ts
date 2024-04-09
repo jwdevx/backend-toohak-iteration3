735b0a1c7bb8aae67ec16e29c26b0105437461f8
Jason Widjaja <z5494973@ad.unsw.edu.au>
2024-04-09 14:56:01 +1000

    helper.ts player.ts player.test.ts: ad player .ts is not yet implemented, i cleaned out code and only does function stub

--- src/player.test.ts
+++ src/player.test.ts
@@ -1,41 +1,6 @@
 test('Remove this test and uncomment the tests below', () => {
   expect(1 + 1).toStrictEqual(2);
 });
-import {
-//   UserCreateReturn,QuizCreateReturn
-  // TODO
-} from './returnInterfaces';
-import {
-//   clear,
-// adminQuizCreate,
-// adminAuthRegister,
-// adminQuestionCreate,
-// adminQuizRemove,
-} from './apiRequestsIter3';
-import {
-// adminQuizThumbnailUpdate,
-// adminQuizViewSessions,
-// adminQuizSessionStart,
-// adminQuizSessionStateUpdate,
-// adminQuizSessionGetStatus,
-// adminQuizSessionGetResults,
-// adminQuizSessionGetResultsCSV,
-} from './apiRequestsIter3';
-import {
-// playerJoin,
-// playerStatus,
-// playerQuestionPositionInfo,
-// playerQuestionAnswerSubmit,
-// playerQuestionResults,
-// playerFinalResults,
-// playerReturnAllChat,
-// playerSendChat
-} from './apiRequestsIter3';
-
-// const ERROR = { error: expect.any(String) };
-// beforeEach(() => {
-//   clear();
-// });
 
 // =============================================================================
 // ====================          playerJoin           ==========================
@@ -53,182 +18,28 @@ import {
 // ===============        playerQuestionPositionInfo          ==================
 // =============================================================================
 
-// TODO proper test, awaiting Venus to finish her function before i can properly test
-
-
-describe('Complete Test for playerQuestionPositionInfo', () => {
-  beforeEach(() => {
-    clear();
-  });
-  // 1.Error 400
-  test('player ID does not exist', () => {
-
-    // const qpInfoPlayer1 = playerQuestionPositionInfo('', 1);
-    // expect(qpInfoPlayer1.statusCode).toStrictEqual(400);
-  });
-
-  test('question position is not valid for the session this player is in', () => {
-
-    /**
-     //Register User
-    let sessionId1 = (adminAuthRegister('hayden.smith@unsw.edu.au', '1234abcd', 'Hayden', 'Smith').jsonBody as UserCreateReturn).token;   
-    
-
-    // //Create Quiz
-    const quizId1 = adminQuizCreate(sessionId1, 'quiz1', 'first quiz');
-    console.log(quizId1);   
-    
-     */
-
-    // const sessionId1 = adminAuthRegister('hayden.smith@unsw.edu.au', '1234abcd', 'Hayden', 'Smith');
-    // console.log(sessionId1);
-    // console.log(sessionId1.token);
-
-
-
-    // let t: UserCreateReturn;
-    // const { jsonBody } = adminAuthRegister('hayden.smith@unsw.edu.au', '1234abcd', 'Hayden', 'Smith');
-    // t = jsonBody as UserCreateReturn;
-    // const quizId1 = adminQuizCreate(t.token, 'quiz1', 'first quiz');
-    // console.log(quizId1);
-
-
-    // //Create question
-    // adminQuestionCreate(sessionId1, quizId1, questionBody1); // TODO change to V2 once implemented
-    // // Start a session
-    // adminQuizSessionStart(sessionId1, quizId1, 5);
-
-
-                // let token1 = (adminAuthRegister('hayden.smith@unsw.edu.au', '1234abcd', 'Hayden', 'Smith').jsonBody as UserCreateReturn).token;
-                // console.log(token1);
-                // // const quizId1 = adminQuizCreate(token1, 'quiz1', 'first quiz');
-
-                // const quizId1= (adminQuizCreate(token1, 'quiz1', 'first quiz').jsonBody as  QuizCreateReturn).quizId;
-                // console.log(quizId1);
-
-/*
-
-    beforeEach(() => {
-      reqClear();
-      token1 = (reqAdminAuthRegister('iloveemails@gmail.com', 'iloveemail1234', 'Ilove', 'Emails').jsonBody as UserCreateReturn).token;
-      token2 = (reqAdminAuthLogin('iloveemails@gmail.com', 'iloveemail1234').jsonBody as UserCreateReturn).token;
-    });
-    
-    test('Successful logout', () => {
-      expect(reqAdminAuthLogoutV2(token1).jsonBody).toStrictEqual({});
-      expect(reqAdminAuthLogoutV2(token2).jsonBody).toStrictEqual({});
-    });
-    
-    test('Error: Token is empty or invalid (does not refer to valid logged in user quiz session)', () => {
-      expect(() => reqAdminAuthLogoutV2('')).toThrow(HTTPError[401]);
-    
-      reqAdminAuthLogoutV2(token1);
-      // token1 is not logged in right now
-      expect(() => reqAdminAuthLogoutV2(token1)).toThrow(HTTPError[401]);
-    });
-  
-  
-    const token1 = (reqAdminAuthRegister('iloveemails@gmail.com', 'iloveemail1234', 'Ilove', 'Emails').jsonBody as UserCreateReturn).token;
-
-*/
-
-    
-    // test('Successful logout', () => {
-    //   expect(reqAdminAuthLogoutV2(token1).jsonBody).toStrictEqual({});
-    //   expect(reqAdminAuthLogoutV2(token2).jsonBody).toStrictEqual({});
-    // });
-    
-    // test('Error: Token is empty or invalid (does not refer to valid logged in user quiz session)', () => {
-    //   expect(() => reqAdminAuthLogoutV2('')).toThrow(HTTPError[401]);
-    
-    //   reqAdminAuthLogoutV2(token1);
-    //   // token1 is not logged in right now
-    //   expect(() => reqAdminAuthLogoutV2(token1)).toThrow(HTTPError[401]);
-    // });
-  
-   
-
-    // //TODO playerJoin
-
-    // //TODO valid player Id byt unvalid question position
-    // // const qpInfoPlayer1 = playerQuestionPositionInfo('', 0);
-    // // expect(viewQuizSession1.statusCode).toStrictEqual(400);
-    // // expect(qpInfoPlayer1 .bodyObj).toStrictEqual(ERROR);
-  });
-  test('session is not currently on this question', () => {
-
-  });
-  test('Session is in LOBBY, QUESTION_COUNTDOWN, or END state', () => {
-
-  });
-  // 3.Success 200
-  test('Success 200', () => {
-
-  });
-});
-
-
-// =============================================================================
-// ================        playerQuestionAnswerSubmit           ================
-// =============================================================================
-
-// TODO proper test, awaiting Venus to finish her function before i can properly test
-/*
-describe('Complete Test for playerQuestionAnswerSubmit', () => {
-  const invalidAnswerIds = [-4, 0];
-  beforeEach(() => {
-    clear();
-  });
-  test('400 if player ID does not exist', () => {
-    // expect(() => playerQuestionAnswerSubmit('', 1,invalidAnswerIds)).toThrow(HTTPError[400]);
-  });
-  test('400 if question position is not valid for the session this player is in', () => {
-
-  });
-  test('400 if session is not in QUESTION_OPEN state', () => {
-
-  });
-  test('400 if session is not yet up to this question', () => {
-
-  });
-
-  test('400 if answer IDs are not valid for this particular question', () => {
-
-  });
-
-  test('400 if there are duplicate answer IDs provided', () => {
-
-  });
-
-  test('400 if less than 1 answer ID was submitted', () => {
-
-  });
-  // 3.Success 200
-  test('Success 200', () => {
-
-  });
-  test('Allow the current player to submit answer(s) to the currently active question.', () => {
-
-  });
-});
-*/
+// TODO JASON proper test, awaiting Venus to finish her function before i can properly test
 
 // =============================================================================
 // =================          playerReturnAllChat          =====================
 // =============================================================================
 
-// TODO ASH
+// TODO SADAT
 
 // =============================================================================
 // ===============          playerFinalResults           =======================
 // =============================================================================
 
-// TODO ASH
+// TODO SADAT
 
 // =============================================================================
 // ================          playerQuestionResults          ====================
 // =============================================================================
 
+// TODO ASH
+
 // =============================================================================
 // ==================          playerSendChat           ========================
 // =============================================================================
+
+// TODO ASH
--- src/player.ts
+++ src/player.ts
@@ -1,211 +1,43 @@
-import HTTPError from 'http-errors';
-import {
-  Questions, state,
-  player, questionResults, Session,
-} from './dataStore';
-import {
-  findAtQuestionMetadata, findQuizSession, hasInvalidOrDuplicateAnswerId,
-  calculateAnswerTime, analyzeAnswer, processAnswerSubmission
-} from './helper';
 import { message } from './dataStore';
-
-
-
 /**
  * To do comment
  */
 export function playerJoin(sessionId: number, name: string): Record<string, never> {
-  // TODO update typescript return types
-  // 1.Error 401
-
-  // 2.Error 403
-
-  // 3.Error 400
-
-  // 4.Success 200
-
   return {};
 }
-
-
 
 /**
  * To do comment
  */
 export function playerStatus(playerId: number): Record<string, never> {
-  // TODO update typescript return types
-  // 1.Error 401
-
-  // 2.Error 403
-
-  // 3.Error 400
-
-  // 4.Success 200
   return {};
 }
 
-
-/**
- * Get the information about a question that the guest player is on. Question position starts at 1
- * @param {number} playerId - an encoded session ID of the user
- * @param {number} quizId - the authenticated quiz ID.
- * @returns {Questions} Questions containing the questions at the question position
- */
-
-export function playerQuestionPositionInfo(playerId: number, questionPosition: number): Questions {
-  const session = findQuizSession(playerId);
-  if (!session) throw HTTPError(400, 'Error player ID does not exist!');
-
-  const question = findAtQuestionMetadata(session, questionPosition);
-  if (!question) throw HTTPError(400, 'Error question position is not valid for this session!');
-  if (session.atQuestion !== questionPosition) {
-    throw HTTPError(400, 'Error session is not currently on this question!');
-  }
-  if (session.state === state.LOBBY || session.state === state.QUESTION_COUNTDOWN || session.state === state.END) {
-    throw HTTPError(400, 'Error session is in LOBBY, QUESTION_COUNTDOWN, or END state!');
-  }
-  const questionPositionInfo = {
-    questionId: question.questionId,
-    question: question.question,
-    duration: question.duration,
-    thumbnailUrl: question.thumbnailUrl,
-    points: question.points,
-    answers: question.answers,
-  };
-  return questionPositionInfo;
+export function playerQuestionPositionInfo(playerId: number, questionPosition: number): Record<string, never> {
+  return {};
 }
 
-/**
- * Allow the current player to submit answer(s) to the currently active question.
- * Question position starts at 1.
- * Note: An answer can be re-submitted once first selection is made, as long as game is in the right state
- *
- * @param {number} playerId - an encoded session ID of the user
- * @param {number} questionPosition - the requested position of question to submit answer
- * @param {Array} answerIds - a number array
- * @returns {} Null if it is successfull else throw error
- */
-export function playerQuestionAnswerSubmit(
-  playerId: number, questionPosition: number, answerIds: number[]): Record<string, never> {
-
-  // Find Session
-  const session = findQuizSession(playerId);
-  if (!session) throw HTTPError(400, 'Error player ID does not exist!');
-
-  // Find Current Question to compare in metadata
-  const question = findAtQuestionMetadata(session, questionPosition);
-  if (!question) throw HTTPError(400, 'Error question position is not valid for this session!');
-
-  // An answer can be re-submitted once first selection is made, as long as game is in the right state
-  if (session.state !== state.QUESTION_OPEN) throw HTTPError(400, 'Error session is not in QUESTION_OPEN state!');
-  if (questionPosition > session.atQuestion) throw HTTPError(400, 'Error session is not yet up to this question!');
-
-  // Checking for invalid or duplicate answer IDs
-  const invalidOrDuplicateAnswerId = hasInvalidOrDuplicateAnswerId(answerIds, question.answers);
-  if (invalidOrDuplicateAnswerId) throw HTTPError(400, 'Error one or more answer IDs are not valid or duplicated!');
-  if (answerIds.length < 1) throw HTTPError(400, 'Error less than 1 answer ID was submitted!');
-  return processAnswerSubmission(playerId, session, question, answerIds, questionPosition);
+export function playerQuestionAnswerSubmit(playerId: number, questionPosition: number, answerIds: number[]): Record<string, never> {
+  return {};
 }
-
-
 /**
  * To do comment
  */
 export function playerQuestionResults(playerId: number, questionPosition: number): Record<string, never> {
-  // TODO update typescript return types
-  // 1.Error 401
-
-  // 2.Error 403
-
-  // 3.Error 400
-
-  // 4.Success 200
-  /*
-  {
-  questionId	        integer
-                      example: 5546
-  playersCorrectList	[This array is ordered in ascending order of player name
-                      string
-                      example: Hayden
-                      List of the name of players
-                      ]
-  averageAnswerTime	integer
-                      example: 45
-                      The average answer time for the question across all players who attempted the question, rounded to the nearest second. 
-                      If no answers are submitted then the value is 0.
-
-  percentCorrect	    integer
-                      example: 54
-                      A percentage rounded to the nearest whole number that describes the percentage of players that got the question completely correct.
-
-  }
-  */
   return {};
 }
-
 
 /**
  * To do comment
  */
 export function playerFinalResults(playerId: number): Record<string, never> {
-  // TODO update typescript return types
-  // 1.Error 401
-
-  // 2.Error 403
-
-  // 3.Error 400
-
-  // 4.Success 200
-  /*
-  {
-  usersRankedByScore	[
-                      A list of all users who played ranked in descending order by score
-                      All scores are rounded to the nearest integer.
-                      If there are players with the same final score, they share the same rank, e.g. players scoring 5, 3, 3, 2, 2, 1 have ranks 1, 2, 2, 4, 4, 6.
-                      {
-                      name	    string
-                                  example: Hayden
-                                  The name of the player that is a top ranker
-                      score	    number
-                                  example: 45
-                                  The final score for the user
-                      }]
-  questionResults     questionId	        integer
-                                          example: 5546
-                      playersCorrectList	[
-                                          This array is ordered in ascending order of player name
-                                          string
-                                          example: Hayden
-                                          List of the name of players
-                                          ]
-                      averageAnswerTime	integer
-                                          example: 45
-                                          The average answer time for the question across all players who attempted the question, rounded to the nearest second. 
-                                          If no answers are submitted then the value is 0.
-
-                      percentCorrect	    integer
-                                          example: 54
-                                          A percentage rounded to the nearest whole number that describes the percentage of players 
-                                          that got the question completely correct.
-  }
-  */
   return {};
 }
-
 
 /**
  * To do comment
  */
 export function playerReturnAllChat(playerId: number): Record<string, never> {
-  // TODO update typescript return types
-  // 1.Error 401
-
-  // 2.Error 403
-
-  // 3.Error 400
-
-  // 4.Success 200
-
   return {};
 }
 
@@ -213,16 +45,7 @@ export function playerReturnAllChat(playerId: number): Record<string, never> {
  * To do comment
  */
 export function playerSendChat(playerId: number, message: message): Record<string, never> {
-  // TODO update typescript return types
-  // 1.Error 401
-
-  // 2.Error 403
-
-  // 3.Error 400
-
-  // 4.Success 200
-
   return {};
 }
 
-//------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
