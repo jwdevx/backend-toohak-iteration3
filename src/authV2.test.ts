c8abfb141dc9dc9ec646e70a8c640a8af2bf004a
Venus Baranwal <z5483461@ad.unsw.edu.au>
2024-04-11 10:31:01 +1000

    Added the testing for adminUserDetailsUpdateV2 in authV2.test.ts

--- src/authV2.test.ts
+++ src/authV2.test.ts
@@ -3,6 +3,7 @@ import { UserCreateReturn, UserDetailsReturn } from './returnInterfaces';
 import {
   adminAuthRegister,
   adminUserDetailsV2,
+  adminUserDetails,
   adminAuthLogoutV2,
   adminUserDetailsUpdateV2,
   adminUserPasswordUpdate,
@@ -58,4 +59,98 @@ describe('Test for adminUserDetailsV2', () => {
 });
 
 // TODO
+describe('Test for adminUserDetailsUpdateV2', () => {
+  beforeEach(() => {
+    clear();
+  });
+  // Success 200
+  test('Check successful updating user: email, NameFirst, nameLast', () => {
+    const token1 = (adminAuthRegister('hayden2@gmail.com', 'iloveemail1234', 'Hayden', 'Smith').bodyObj as UserCreateReturn).token;
+    expect(token1).toStrictEqual(expect.any(String));
+    const retVal = adminUserDetailsUpdateV2(token1, 'smith.hayden@unsw.edu.au', 'Angelina', 'Jolie');
+    expect(retVal.bodyObj).toStrictEqual({});
+
+    const userDetailsRes = adminUserDetails(token1);
+    expect(userDetailsRes.bodyObj).toStrictEqual({
+      user: {
+        userId: expect.any(Number),
+        name: 'Angelina Jolie',
+        email: 'smith.hayden@unsw.edu.au',
+        numSuccessfulLogins: 1,
+        numFailedPasswordsSinceLastLogin: 0
+      },
+    });
+  });
+  // Error 401 - 'Token is empty or not provided'
+  test('Token is empty or not provided', () => {
+    const token1 = (adminAuthRegister('hayden2@gmail.com', 'iloveemail1234', 'Hayden', 'Smith').bodyObj as UserCreateReturn).token;
+    expect(token1).toStrictEqual(expect.any(String));
+    expect(() => adminUserDetailsUpdateV2(null, 'smith.hayden@unsw.edu.au', 'Angelina', 'Jolie'))
+      .toThrow(HTTPError[401]);
+    expect(() => adminUserDetailsUpdateV2(undefined, 'smith.hayden@unsw.edu.au', 'Angelina', 'Jolie'))
+      .toThrow(HTTPError[401]);
+    expect(() => adminUserDetailsUpdateV2('', 'smith.hayden@unsw.edu.au', 'Angelina', 'Jolie'))
+      .toThrow(HTTPError[401]);
+  });
+  test('Token is invalid (does not refer to valid logged in user session)', () => {
+    const token1 = (adminAuthRegister('hayden2@gmail.com', 'iloveemail1234', 'Hayden', 'Smith').bodyObj as UserCreateReturn).token;
+    expect(token1).toStrictEqual(expect.any(String));
+    expect(() => adminUserDetailsUpdateV2('99999999', 'smith.hayden@unsw.edu.au', 'Angelina', 'Jolie')).toThrow(HTTPError[401]);
+  });
+  // Error 400
+  test('Check error message if email is currently used by another user', () => {
+    const token1 = (adminAuthRegister('hayden2@gmail.com', 'iloveemail1234', 'Hayden', 'Smith').bodyObj as UserCreateReturn).token;
+    expect(token1).toStrictEqual(expect.any(String));
+    adminAuthRegister('taylor.swift@unsw.edu.au', '5678efgh', 'Taylor', 'Swift');
+    expect(() => adminUserDetailsUpdateV2(token1, 'taylor.swift@unsw.edu.au', 'Jonathan', 'Swift')).toThrow(HTTPError[400]);
+  });
+  test('Missing parameters', () => {
+    const token1 = (adminAuthRegister('hayden2@gmail.com', 'iloveemail1234', 'Hayden', 'Smith').bodyObj as UserCreateReturn).token;
+    expect(token1).toStrictEqual(expect.any(String));
+    expect(() => adminUserDetailsUpdateV2(token1, '  ', 'Hayden', 'Smith')).toThrow(HTTPError[400]);
+    expect(() => adminUserDetailsUpdateV2(token1, '1234abcd', '', 'Smith')).toThrow(HTTPError[400]);
+    expect(() => adminUserDetailsUpdateV2(token1, '1234abcd', 'Hayden', '')).toThrow(HTTPError[400]);
+  });
+  test.each([
+    ['invalid_email'],
+    ['email.com'],
+    ['email@'],
+    ['email@hayden'],
+    ['hayden@@gmail.com'],
+  ])('update email to %s should return 400 error', async (newEmail) => {
+    const token = (adminAuthRegister('hayden2@gmail.com', 'iloveemail1234', 'Hayden', 'Smith').bodyObj as UserCreateReturn).token;
+    expect(() => adminUserDetailsUpdateV2(token, newEmail, 'Hayden', 'Smith')).toThrow(HTTPError[400]);
+  });
+  test.each([
+    ['Ha*yden'],
+    ['Hayden7'],
+    ['Hayden&Smith'],
+    ['H@yden'],
+  ])('Check error message for invalid characters in NameFirst: %s', async (invalidName) => {
+    const token = (adminAuthRegister('hayden2@gmail.com', 'iloveemail1234', 'Hayden', 'Smith').bodyObj as UserCreateReturn).token;
+    expect(() => adminUserDetailsUpdateV2(token, '1234abcd', invalidName, 'Smith')).toThrow(HTTPError[400]);
+  });
+  test('Check error message for NameFirst less than 2 characters or more than 20 characters', () => {
+    const token = (adminAuthRegister('hayden2@gmail.com', 'iloveemail1234', 'Hayden', 'Smith').bodyObj as UserCreateReturn).token;
+    expect(() => adminUserDetailsUpdateV2(token, 'hayden1@gmail.com', 'H', 'Smith')).toThrow(HTTPError[400]);
+    expect(() => adminUserDetailsUpdateV2(token, 'hayden2@gmail.com', 'Haydenhaydenhaydenhayden', 'Smith')).toThrow(HTTPError[400]);
+  });
+  test.each([
+    ['Sm_ith'],
+    ['Sm*ith'],
+    ['Smith3'],
+    ['Sm!th'],
+  ])('Check error message for invalid characters in NameLast: %s', async (invalidName) => {
+    const token = (adminAuthRegister('hayden2@gmail.com', 'iloveemail1234', 'Hayden', 'Smith').bodyObj as UserCreateReturn).token;
+    expect(() => adminUserDetailsUpdateV2(token, '1234abcd', 'Hayden', invalidName)).toThrow(HTTPError[400]);
+  });
+  test.each([
+    ['s'],
+    ['Smithsmithsmithsmithsmith'],
+  ])('Check error message for NameLast length: %s', async (invalidLastName) => {
+    const token = (adminAuthRegister('hayden2@gmail.com', 'iloveemail1234', 'Hayden', 'Smith').bodyObj as UserCreateReturn).token;
+    expect(() => adminUserDetailsUpdateV2(token, 'hayden7@gmail.com', 'Hayden', invalidLastName)).toThrow(HTTPError[400]);
+  });
+});
+
 
