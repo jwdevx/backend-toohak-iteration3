dda976b2d041ead901603c0f54a349db5cc421c0
Venus Baranwal <z5483461@ad.unsw.edu.au>
2024-04-11 10:13:23 +1000

    Added the V2 testing for adminAuthLogout in authV2.test.ts for apiRequestsIter3.ts

--- src/authV2.test.ts
+++ src/authV2.test.ts
@@ -1,3 +1,32 @@
-test('Remove this test and uncomment the tests below', () => {
-  expect(1 + 1).toStrictEqual(2);
+import HTTPError from 'http-errors';
+
+import {
+  adminAuthLogin,
+  adminAuthRegister,
+  adminUserDetails,
+  adminAuthLogoutV2,
+  adminUserDetailsUpdate,
+  adminUserPasswordUpdate,
+  clear,
+} from './apiRequestsIter3';
+
+describe('Test for adminAuthLogoutV2', () => {
+  beforeEach(() => {
+    clear();
+  });
+  test('Empty token', () => {
+    // Error 401 Logout Test Start Here
+    expect(() => adminAuthLogoutV2('')).toThrow(HTTPError[401]);
+  });
+  test('Empty token', () => {
+    expect(() => adminAuthLogoutV2('9999999')).toThrow(HTTPError[401]);
+  });
+  test('success 200', () => {
+    // Register Hayden - Session 1
+    const token2 = (adminAuthRegister('iloveemails@gmail.com', 'iloveemail1234', 'Ilove', 'Emails').bodyObj as UserCreateReturn).token;
+    expect(token2).toStrictEqual(expect.any(String));
+    const logout = adminAuthLogoutV2(token2);
+    expect(logout.bodyObj).toStrictEqual({});
+  });
 });
+
